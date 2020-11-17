597d0c81d470a9157db608fa41213a2e568c003a
Sandeep Verma <sandeepverma4321@gmail.com>
2020-11-17 15:41:30 +0530

    Added the data for filters and shop items from api.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,14 @@ export class ShopService {
   constructor(private http: HttpClient) {}
 
   getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+    return this.http.get<IPagination>(`${this.baseUrl}products?pageSize=50`);
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(`${this.baseUrl}products/brands`);
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(`${this.baseUrl}products/types`);
   }
 }
