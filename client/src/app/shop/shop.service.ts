087e9537a4867bc3c62fa63cae689fdf2e3dd59d
Sandeep Verma <sandeepverma4321@gmail.com>
2020-11-17 19:06:07 +0530

    Adding sort, search and paging functionality.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,19 +1,46 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
+import { delay, map } from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) {}
 
-  getProducts() {
-    return this.http.get<IPagination>(`${this.baseUrl}products?pageSize=50`);
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams();
+    if (shopParams.brandId !== 0) {
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+    if (shopParams.typeId !== 0) {
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageIndex', shopParams.pageSize.toString());
+
+    return this.http
+      .get<IPagination>(`${this.baseUrl}products`, {
+        observe: 'response',
+        params,
+      })
+      .pipe(
+        // pipe allows multiple rxjs operators to chain
+        // delay(1000), if want a delay for whatever reason then add from rxjs operators
+        map((response) => {
+          return response.body;
+        })
+      );
   }
 
   getBrands() {
